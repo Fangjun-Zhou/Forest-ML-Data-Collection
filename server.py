e97f05e305a124c9a4e78611131afa92f09e8f29
Fangjun Zhou <fzhou48@wisc.edu>
2022-03-08 22:58:32 -0600

    Update: server.py Finish question generation.

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 from datetime import datetime
 import os
+import random
 from flask import Flask, jsonify, request, Response
+import pandas
 
 AUDIO_DIR = "standard_set"
 
 
 audioList = os.listdir(AUDIO_DIR)
+
+ansDf = pandas.read_csv("standard_ans.csv")
+ansDf = ansDf.set_index("audio")
 
 app = Flask(__name__)
 
@@ -17,6 +22,11 @@ def get_time():
 
 @app.route("/api/audio")
 def get_audio():
+    """Get a specific audio file from the audio list
+
+    Returns:
+        Response: network response
+    """
     audioFileName = request.args.get("name")
     if not audioFileName in audioList:
         return Response("Bad Request: Audio file not found", status=400)
@@ -25,6 +35,46 @@ def get_audio():
         content = af.read()
 
     return Response(content, content_type="audio/mpeg")
+
+
+@app.route("/api/randquestion")
+def random_question():
+    """Randomly generate a question
+
+    Returns:
+        Response: network response
+    """
+    # Random choose a audio.
+    audioFileName = random.choice(audioList)
+    print(f"Choose {audioFileName}")
+
+    # Get rid of the file extention
+    key = audioFileName.split(".")[0]
+
+    # Answer not found
+    if not key in list(ansDf.index):
+        print("No answer found in answer data frame.")
+        return Response("Answer found error", status=400)
+
+    # The real answer of the question
+    ans = int(ansDf.at[key, "ans"])
+    # Add two more bait answers
+    residents = [int(x) for x in set(ansDf["ans"]) if x != ans]
+    random.shuffle(residents)
+    if len(residents) > 2:
+        choices = residents[0:2]
+    else:
+        choices = residents
+    choices.append(ans)
+
+    question = {
+        "audio": audioFileName,
+        "choices": choices,
+    }
+
+    print(question)
+
+    return jsonify(question)
 
 
 if __name__ == '__main__':
