57feaf502d995334f998743a003d1106b52df4fc
Fangjun Zhou <fzhou48@wisc.edu>
2022-03-08 22:18:57 -0600

    Update: server.py Added sample audio api.

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from datetime import datetime
-from flask import Flask, jsonify
+import os
+from flask import Flask, jsonify, request, Response
 
+AUDIO_DIR = "standard_set"
+
+
+audioList = os.listdir(AUDIO_DIR)
 
 app = Flask(__name__)
 
@@ -8,6 +13,18 @@ app = Flask(__name__)
 @app.route("/api/time")
 def get_time():
     return jsonify(datetime.now())
+
+
+@app.route("/api/audio")
+def get_audio():
+    audioFileName = request.args.get("name")
+    if not audioFileName in audioList:
+        return Response("Bad Request: Audio file not found", status=400)
+
+    with open(os.path.join(AUDIO_DIR, audioFileName), "rb") as af:
+        content = af.read()
+
+    return Response(content, content_type="audio/mpeg")
 
 
 if __name__ == '__main__':
